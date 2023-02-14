f082852ca023e9fbb8e33c7261813e7b2b516b3d
DulcePC <dulcefrancelispc@gmail.com>
2023-02-14 08:38:52 -0400

    feat: :sparkles: change titles

--- components/population/home/Join.vue
+++ components/population/home/Join.vue
@@ -4,10 +4,10 @@
       <img src="img/home/join.png" alt="Join us" class="lg:max-w-[822px] w-full xl:m-0 mx-auto" />
     </figure>
     <div>
-      <h2 class="lg:text-6xl lg:leading-[68px] text-3xl font-bold text-primary mb-4 md:text-left text-center">Únete al <br class="xl:block hidden" /> equipo</h2>
+      <h2 class="lg:text-6xl lg:leading-[68px] text-3xl font-bold text-primary mb-4 md:text-left text-center">Afíliate <br class="xl:block hidden" /> a nosotros</h2>
       <p class="lg:text-2xl text-base text-secondary font-medium mb-4 md:text-left text-center">Conoce los beneficios.</p>
       <p class="text-sm font-normal text-black mb-7">¡Únase a nuestro programa de afiliación y comience a ganar dinero por cada afiliación y venta que haga! Simplemente cree su cuenta y observe crecer el saldo de su cuenta a medida que sus invitados se convierten en sus afiliados y estos a su vez afilian o venden publicidad a sus clientes</p>
-      <NuxtLink to="/join-us" class="btn-secondary h-9 w-max">Únete al equipo</NuxtLink>
+      <NuxtLink to="/join-us" class="btn-secondary h-9 w-max">Afíliate</NuxtLink>
     </div>
   </section>
 </template>
