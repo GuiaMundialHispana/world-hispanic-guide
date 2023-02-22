368c7a6c89deb92e2b277cefc00760b80c5f8ba7
DulcePC <dulcefrancelispc@gmail.com>
2023-02-22 11:06:49 -0400

    feat: :sparkles: add error page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="py-14">
+    <div class="container">
+      <figure class="mb-20">
+        <img src="/img/error.jpg" alt="Under construction" class="max-w-[695px] mx-auto">
+      </figure>
+      <h1 class="text-4xl text-primary font-bold text-center mb-2">Oops, <span class="text-secondary">página no encontrada</span></h1>
+      <p class="text-sm text-black text-center mb-4">Vuelve a intentarlo más tarde</p>
+      <NuxtLink to="/" class="btn px-4 mx-auto w-max h-9">Ir al home</NuxtLink>
+    </div>
+  </section>
+</template>
