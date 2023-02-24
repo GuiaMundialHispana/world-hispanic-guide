d4af4b7d2635e950216912b078f0aa36f7471242
DulcePC <dulcefrancelispc@gmail.com>
2023-02-23 20:25:48 -0400

    fix: :bug: change team information

--- assets/helpers/Team.js
+++ assets/helpers/Team.js
@@ -1,106 +1,52 @@
 export const teams = [
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
-  {
-    img: 'team-placeholder.jpg',
-    name: 'Nombre y apellido',
-    position: 'CEO Guía Mundial Hispana',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
-  },
   // {
-  //   img: 'rafael-jaquez.jpg',
-  //   name: 'Rafael Jaquez',
+  //   img: 'team-placeholder.jpg',
+  //   name: 'Nombre y apellido',
   //   position: 'CEO Guía Mundial Hispana',
-  //   description: 'Empresario Dominicano y Presidente Fundador de Guía Mundial Hispana. Amplio rango de responsabilidades en el diseño, planificación y ejecución de las estrategias de la empresa.'
+  //   description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type'
   // },
+  {
+    img: 'rafael-jaquez.jpg',
+    name: 'Rafael Jaquez',
+    position: 'CEO Guía Mundial Hispana',
+    description: 'Empresario Dominicano y Presidente Fundador de Guía Mundial Hispana. Amplio rango de responsabilidades en el diseño, planificación y ejecución de las estrategias de la empresa.'
+  },
   // {
   //   img: 'ramon-rodriguez.jpg',
   //   name: 'Ramon Rodriguez',
   //   position: 'Vicepresidente ejecutivo y Cofundador',
   //   description: 'Mi meta es lograr una completa integración de todos los procesos de la empresa apoyándome en las nuevas tecnologías con el objetivo de mejorar el rendimiento interno.'
   // },
-  // {
-  //   img: 'shadda-jaquez.jpg',
-  //   name: 'Shaddai Jaquez',
-  //   position: 'Gerente de Finanzas y Cofundadora',
-  //   description: 'Gerente de finanzas tiene la responsabilidad del manejo de las finanzas de Guía Mundial Hispana en todo el mundo.'
-  // },
-  // {
-  //   img: 'aurelio-mendoza.jpg',
-  //   name: 'Aurelio Henriquez Mendoza',
-  //   position: 'Gerente General y Cofundador',
-  //   description: 'Tiene bajo su responsabilidad coordinar las operaciones con su equipo de ejecutivos.'
-  // },
+  {
+    img: 'shadda-jaquez.jpg',
+    name: 'Shaddai Jaquez',
+    position: 'Gerente de Finanzas y Cofundadora',
+    description: 'Gerente de finanzas tiene la responsabilidad del manejo de las finanzas de Guía Mundial Hispana en todo el mundo.'
+  },
+  {
+    img: 'aurelio-mendoza.jpg',
+    name: 'Aurelio Henriquez Mendoza',
+    position: 'Gerente General y Cofundador',
+    description: 'Tiene bajo su responsabilidad coordinar las operaciones con su equipo de ejecutivos.'
+  },
   // {
   //   img: 'sendy-rodriguez.png',
   //   name: 'Sendy Amelia Rodriguez',
   //   position: 'Gerente de Finanzas y Cofundadora',
   //   description: 'Gerente de finanzas, tiene una de las posiciones d.e mayor responsabilidad dentro de Guía Mundial Hispana'
   // },
-  // {
-  //   img: 'pedro-matos.jpg',
-  //   name: 'Pedro Matos',
-  //   position: 'Gerente de Informática y Cofundador',
-  //   description: 'Responsable por el manejo de la seguridad del sistema utilizado por la empresa.'
-  // },
-  // {
-  //   img: 'henry-paniagua.jpg',
-  //   name: 'Henry Paniagua',
-  //   position: 'Gerente de Publicidad y cofundador',
-  //   description: 'Trabajo en el departamento de divulgación técnica de la secretaria de estado de agricultura. Constituyó la primera empresa de letreros y vallas del sur.'
-  // },
+  {
+    img: 'pedro-matos.jpg',
+    name: 'Pedro Matos',
+    position: 'Gerente de Informática y Cofundador',
+    description: 'Responsable por el manejo de la seguridad del sistema utilizado por la empresa.'
+  },
+  {
+    img: 'henry-paniagua.jpg',
+    name: 'Henry Paniagua',
+    position: 'Gerente de Publicidad y cofundador',
+    description: 'Trabajo en el departamento de divulgación técnica de la secretaria de estado de agricultura. Constituyó la primera empresa de letreros y vallas del sur.'
+  },
   // {
   //   img: 'elizabeth-matos.jpg',
   //   name: 'Elizabeth Matos',
