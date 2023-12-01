9d2db184ea67c389202a249d679aed7fe729aa60
DulcePC <d.perez@kation.com.do>
2023-12-01 10:38:00 -0400

    :sparkles: add links in logos

--- components/population/home/Companies.vue
+++ components/population/home/Companies.vue
@@ -3,30 +3,30 @@
     <div class="2xl:container">
       <div class="lg:w-[600px] text-center mx-auto">
         <h2 class="text-primary text-[28px] mb-7 font-medium">Nuestras empresas</h2>
-        <p class="lg:mx-auto mx-4 mb-14 text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        <p class="lg:mx-auto mx-4 mb-14 text-sm">Descubre nuestras empresas, dedicadas día tras día a brindar satisfacción a nuestros usuarios.
         </p>
       </div>
       <div class="grid grid-cols-2 lg:grid-cols-4 lg:justify-between gap-8 lg:px-20 px-4 mx-auto">
-        <a href="#">
+        <a href="https://hispana-home-nuxt-eight.vercel.app/" target="_blank">
           <figure class="px-4">
             <img class="md:w-52 lg:w-60 mx-auto" src="/img/home/hispana-home.png" alt="Hispana Home">
           </figure>
         </a>
-        <a href="#">
+        <a href="https://hispana-negocios.vercel.app/" target="_blank">
           <figure class="px-4">
             <img class="md:w-52 lg:w-60 mx-auto" src="/img/home/hispana-bussiness.png" alt="Hispana Negocios">
           </figure>
         </a>
-        <a href="#">
+        <a href="https://hispana-auto-nuxt.vercel.app/" target="_blank">
           <figure class="px-4">
             <img class="md:w-52 lg:w-60 mx-auto" src="/img/home/hispana-car.png" alt="Hispana Auto">
           </figure>
         </a>
-        <a href="#">
+        <NuxtLink to="/">
           <figure class="px-4">
             <img class="md:w-52 lg:w-60 mx-auto" src="/img/home/hispana-afiliates.png" alt="Hispana Afiliados">
           </figure>
-        </a>
+        </NuxtLink>
       </div>
     </div>
   </section>
