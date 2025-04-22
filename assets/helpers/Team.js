0b9ebd9ca7453d9f322ce8ace0cdbcdd917b1268
dulcepc <d.perez@kation.com.do>
2025-04-22 19:43:05 -0400

    :sparkles: add team members with images and descriptions

--- assets/helpers/Team.js
+++ assets/helpers/Team.js
@@ -10,5 +10,17 @@ export const teams = [
     name: 'Pedro Matos',
     position: 'Gerente de Informática y Cofundador',
     description: 'Responsable por el manejo de la seguridad del sistema utilizado por la empresa.'
-  }
+  },
+  {
+    img: 'aurelio-mendoza.jpg',
+    name: 'Aurelio Henriquez Mendoza',
+    position: 'Gerente General y Cofundador',
+    description: 'Tiene bajo su responsabilidad coordinar las operaciones con su equipo de ejecutivos.'
+  },
+  {
+    img: 'henry-paniagua.jpg',
+    name: 'Henry Paniagua',
+    position: 'Gerente de Publicidad y cofundador',
+    description: 'Trabajo en el departamento de divulgación técnica de la secretaria de estado de agricultura. Constituyó la primera empresa de letreros y vallas del sur.'
+  },
 ];
