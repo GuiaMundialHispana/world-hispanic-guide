23da2f6ed30c6a3bfc1b82c80515be8927f2882a
dulcepc <d.perez@kation.com.do>
2025-04-24 16:44:26 -0400

    :sparkles: implement tabbed view for active and inactive affiliates

--- components/population/home/Companies.vue
+++ components/population/home/Companies.vue
@@ -28,23 +28,54 @@
           </figure>
         </NuxtLink>
       </div>
-      <ul class="grid grid-cols-2 lg:grid-cols-4 lg:justify-between gap-8 lg:px-20 px-4 mt-10 mx-auto">
-        <li v-for="team in teams" :key="team">
-          <MoleculesTeamCard :name="team.name" :img="team.img" />
+    </div>
+    <div class="container max-w-4xl mt-10">
+      <h2 class="text-primary text-[28px] mb-7 font-medium text-center">Conozca nuestros afiliados</h2>
+      <div class="flex items-center gap-4 justify-center">
+        <button class="btn" @click="currentTab = 'active'" :class="{active: currentTab === 'active'}">Active</button>
+        <button class="btn" @click="currentTab = 'inactive'" :class="{active: currentTab === 'inactive'}">Inactive</button>
+      </div>
+      <ul v-if="currentTab === 'active'" class="mt-10">
+        <li v-for="team in actives" :key="team" class="flex items-center gap-2 mb-6">
+          <NuxtImg :src="`/img/team/${img}`" :alt="name" placeholder="/img/team/team-placeholder.jpg" class="w-14 h-14 object-cover object-center rounded-full" />
+          <h4 class="font-medium text-black text-xl">{{ team.name }}</h4>
         </li>
       </ul>
-
+      <ul v-if="currentTab === 'inactive'"  class="mt-10">
+        <li v-for="team in inactive" :key="team" class="flex items-center gap-2 mb-6">
+          <NuxtImg :src="`/img/team/${img}`" :alt="name" placeholder="/img/team/team-placeholder.jpg" class="w-14 h-14 object-cover object-center rounded-full" />
+          <h4 class="font-medium text-black text-xl">{{ team.name }}</h4>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-const config = useRuntimeConfig()
-const { data:teams } = useFetch('affiliates', {
+const config = useRuntimeConfig();
+const actives = ref([]);
+const inactive = ref([]);
+const currentTab = ref('active');
+const { data } = useFetch('affiliates', {
   method: 'GET',
   baseURL: config.public.API,
-  transform(_data) {
-    return _data.results
+  transform(data) {
+    data.results.forEach((item: {}) => {
+      if (item.affiliate_type_id === 1) {
+        actives.value.push(item);
+      } else {
+        inactive.value.push(item);
+      }
+    })
   }
 })
 </script>
+
+<style lang="postcss" scoped>
+.btn {
+  @apply flex border border-solid border-primary bg-white hover:bg-primary hover:text-white w-full h-[40px] justify-center px-5 py-3 text-primary text-sm font-bold rounded-lg items-center;
+  &.active {
+    @apply bg-primary text-white;
+  }
+}
+</style>
