502d1887c8f887d6aca175cf302c758d459cd492
dulcepc <d.perez@kation.com.do>
2025-07-25 09:16:18 -0400

    :art: update team section title to 'Miembros del cómite ejecutivo de REP.DOM' in Team.vue

--- components/population/About/Team.vue
+++ components/population/About/Team.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="2xl:max-w-screen-2xl mx-auto px-4 md:px-8 p-8 lg:pb-16 lg:px-16 text-center lg:text-left">
-    <h2 class="text-[28px] font-medium">Nuestro equipo</h2>
+    <h2 class="text-[28px] font-medium">Miembros del cómite ejecutivo de REP.DOM</h2>
     <hr class="mt-2 border-#F5F5F5 mb-14">
     <ul class="list-none p-0 m-0 grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 xl:gap-14 gap-6">
       <li v-for="team in teams" :key="team">
